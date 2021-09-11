bf55b3eaf963e90dcabe682c0d026933e904f2d6
Adriana <am20dipi@alum.siena.edu>
2021-09-11 11:23:34 -0400

    seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Post.create(name: 'Essay on Cats', text: 'Essay on CatsEssay on CatsEssay on CatsEssay on CatsEssay on Cats', likes: 1)
+Post.create(name: 'A Dream About Summer', text: 'A Dream About SummerA Dream About SummerA Dream About SummerA Dream About SummerA Dream About Summer', likes: 1)
+Post.create(name: 'First Day of School: I Hated It', text: 'First Day of School: I Hated ItFirst Day of School: I Hated ItFirst Day of School: I Hated ItFirst Day of School: I Hated ItFirst Day of School: I Hated It', likes: 1)
+
+
+Image.create(name: "Van Gogh's Sunflowers", source: "www.vangogh.com/sunflowers", caption: 'Acrylic on Canvas', likes: 2)
+Image.create(name: "The Sistine Chapel", source: "www.sistinechapel.com", caption: "Michelangelo's greatest work", likes: 2)
+Image.create(name: "Van Gogh's Self Portratir", source: "www.vangogh.com/self-portrait", caption: 'Acrylic on Canvas', likes: 2)
+
+Video.create(name: 'Art Explained: The Garden of Earthly Delights', source: 'www.youtube.com/garden', likes: 3)
+Video.create(name: 'Art Explained: Basquiat', source: 'www.youtube.com/basquiat', likes: 3)
