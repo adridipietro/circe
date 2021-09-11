068a5468041a4df225d637b54626737737a58dec
Adriana <am20dipi@alum.siena.edu>
2021-09-11 11:48:59 -0400

    building out actions/index.js + actions.types.js; creating files for each resource to have actions

---
+++ frontend/src/actions/index.js
@@ -0,0 +1,24 @@
+export {
+    getImages,
+    createImage,
+    deleteImage,
+    editImage,
+    likeImage,
+} from './images'
+
+
+export {
+    getVideos,
+    createVideo,
+    deleteVideo,
+    editVideo,
+    likeVideo,
+} from './videos'
+
+export {
+    getPosts,
+    createPost,
+    deletePost,
+    editPost,
+    likePost,
+} from './posts'
---
+++ frontend/src/actions/types.js
@@ -0,0 +1,22 @@
+export const LOADING_IMAGES = 'LOADING_IMAGES'
+export const CREATE_IMAGE = 'CREATE_IMAGE'
+export const GET_IMAGES = 'GET_IMAGES'
+export const DELETE_IMAGE = 'DELETE_IMAGE'
+export const LIKE_IMAGE = 'LIKE_IMAGE'
+export const EDIT_IMAGE = 'EDIT_IMAGE'
+
+
+export const LOADING_VIDEOS = 'LOADING_VIDEOS'
+export const CREATE_VIDEO = 'CREATE_VIDEO'
+export const GET_VIDEOS = 'GET_VIDEOS'
+export const DELETE_VIDEO = 'DELETE_VIDEO'
+export const LIKE_VIDEO = 'LIKE_VIDEO'
+export const EDIT_VIDEO = 'EDIT_VIDEO'
+
+
+export const LOADING_POSTS = 'LOADING_POSTS'
+export const CREATE_POST = 'CREATE_POST'
+export const GET_POSTS = 'GET_POSTS'
+export const DELETE_POST = 'DELETE_POST'
+export const LIKE_POST = 'LIKE_POST'
+export const EDIT_POST = 'EDIT_POST'
