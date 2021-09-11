484d2fc7ef0746295ad13504e6bcb75e1e7f42ff
Adriana <am20dipi@alum.siena.edu>
2021-09-10 20:22:56 -0400

    Dropping + recreating database; notes.md file; adding gems

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -15,26 +15,6 @@ ActiveRecord::Schema.define(version: 2021_09_11_002050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bookmarks", force: :cascade do |t|
-    t.string "headline"
-    t.string "web_url"
-    t.string "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "favorite"
-    t.bigint "category_id"
-    t.integer "user_id", null: false
-    t.index ["category_id"], name: "index_bookmarks_on_category_id"
-    t.index ["user_id"], name: "index_bookmarks_on_user_id"
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_categories_on_user_id"
-  end
 
   create_table "images", force: :cascade do |t|
     t.string "source"
@@ -53,20 +33,6 @@ ActiveRecord::Schema.define(version: 2021_09_11_002050) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email", default: "", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "jti", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
 
   create_table "videos", force: :cascade do |t|
     t.string "source"
@@ -77,6 +43,4 @@ ActiveRecord::Schema.define(version: 2021_09_11_002050) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "bookmarks", "categories"
-  add_foreign_key "categories", "users"
 end
