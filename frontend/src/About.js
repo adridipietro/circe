2aa5bd0c42799eaddb2a5abc7cc3bcc5a848e81d
Adriana <am20dipi@alum.siena.edu>
2021-09-18 10:37:53 -0400

    About page content

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div className="about-container">
-            <h3>ABOUT</h3>
-            <p>...</p>
-        </div>
+        <>
+        <br/>
+            <div className="about-container">
+                <h2>WELCOME TO CIRCE</h2>
+                <p>...</p>
+                <p>CIRCE is an online software resource designed to share and store artistic content.</p>
+                <p>Here, you can organize everything you have ever created, valued or been inspired from.</p>
+                <p>Keep what is important to you close with CIRCE</p>
+                <p>...</p>
+                <h3>How It Works</h3>
+                <ul>
+                    <li>Collect anything: Save images, texts, links, entries...</li><br/>
+                    <li>Share ideas with other artists or collectors alike.</li><br/>
+                    <li>Find inspiration or share inspiration.</li>
+                </ul>
+            </div>
+        </>
     )
 }
